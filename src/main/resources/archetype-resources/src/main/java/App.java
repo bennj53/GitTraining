999d7d16c9b38c3cbb77754c063fc972fe68ad50
JULLIOT Benoît <benoitjulliot@gmail.com>
2023-12-24 11:19:52 +0100

    init project

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.whiterabbits;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
